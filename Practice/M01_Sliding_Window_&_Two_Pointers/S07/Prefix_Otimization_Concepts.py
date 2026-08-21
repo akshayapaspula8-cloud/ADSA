f56e6a14803037ecdbacaa656f5c724cfc1d1ae7
Paspula Akshaya <akshayapaspula8@gmail.com>
2026-08-21 13:25:05 +0530

    msg(1)

--- Practice/M01_Sliding_Window_&_Two_Pointers/S07/Prefix_Otimization_Concepts.py
+++ Practice/M01_Sliding_Window_&_Two_Pointers/S07/Prefix_Otimization_Concepts.py
@@ -40,7 +40,6 @@ class Solution:
         left_sum = 0
         
         for i, num in enumerate(nums):
-            # right_sum = total_sum - left_sum - num
             if left_sum == total_sum - left_sum - num:
                 return i
             left_sum += num
